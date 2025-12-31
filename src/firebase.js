c9d565fa43ef260b55543a79d593c211e6c5f931
MrNihalT <nihal.chiyoor@gmail.com>
2025-12-31 13:18:39 +0530

    crucial commit env file

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Replace the following configuration with your own Firebase project configuration
 // You can find this in the Firebase Console -> Project Settings -> General -> Your apps -> SDK Setup and Configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCKKJgOwMkRQFQaUR-cjw5MLFAlYK4wR0Y",
-    authDomain: "igfest-7b66e.firebaseapp.com",
-    projectId: "igfest-7b66e",
-    storageBucket: "igfest-7b66e.firebasestorage.app",
-    messagingSenderId: "42519337756",
-    appId: "1:42519337756:web:1aff632f1942c32555f8d9",
-    measurementId: "G-QTHSZ2K98Y",
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
